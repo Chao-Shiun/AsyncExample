77d5dd3e610889488c68db3f277b689f01b6e84a
Charles <e4366.tw@gmail.com>
2019-07-03 00:18:14 +0800

    使用nameof顯示方法名稱

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -59,28 +59,28 @@ namespace ConsoleApp4
 
 
             #region Many await sync flow
-            Console.WriteLine("準備執行AwaitMethodAAsync");
-            var result1 = await AwaitMethodAAsync(1);
-            Console.WriteLine("準備執行AwaitMethodBAsync");
-            var result2 = await AwaitMethodBAsync(2);
-            Console.WriteLine("準備執行AwaitMethodCAsync");
-            var result3 = await AwaitMethodCAsync(3);
+            Console.WriteLine($"準備執行{nameof(AwaitMethod_A_Async)}");
+            var result1 = await AwaitMethod_A_Async(1);
+            Console.WriteLine($"準備執行{nameof(AwaitMethod_B_Async)}");
+            var result2 = await AwaitMethod_B_Async(2);
+            Console.WriteLine($"準備執行{nameof(AwaitMethod_C_Async)}");
+            var result3 = await AwaitMethod_C_Async(3);
 
             Console.WriteLine(result1);
             Console.WriteLine(result2);
             Console.WriteLine(result3);
-            
+
 
             #endregion
 
 
             #region Many await async flow
-            //Console.WriteLine("準備執行AwaitMethodAAsync");
-            //var result1 = AwaitMethodAAsync(1);
-            //Console.WriteLine("準備執行AwaitMethodBAsync");
-            //var result2 = AwaitMethodBAsync(2);
-            //Console.WriteLine("準備執行AwaitMethodCAsync");
-            //var result3 = AwaitMethodCAsync(3);
+            //Console.WriteLine($"準備執行{nameof(AwaitMethod_A_Async)}");
+            //var result1 = AwaitMethod_A_Async(1);
+            //Console.WriteLine($"準備執行{nameof(AwaitMethod_B_Async)}");
+            //var result2 = AwaitMethod_B_Async(2);
+            //Console.WriteLine($"準備執行{nameof(AwaitMethod_B_Async)}");
+            //var result3 = AwaitMethod_C_Async(3);
 
             //var waitAll = Task.WhenAll(result1, result2, result3);
 
@@ -107,27 +107,27 @@ namespace ConsoleApp4
             return DateTime.Now.ToString($"{no} mm:ss fff");
         }
 
-        public static async Task<string> AwaitMethodAAsync(int no)
+        public static async Task<string> AwaitMethod_A_Async(int no)
         {
-            Console.WriteLine($"AwaitMethodAAsync 執行 await前 ThreadId：{Thread.CurrentThread.ManagedThreadId}");
+            Console.WriteLine($"{nameof(AwaitMethod_A_Async)} 執行 await前 ThreadId：{Thread.CurrentThread.ManagedThreadId}");
             string result = await GetMillisecondAsync(no);
-            Console.WriteLine($"AwaitMethodAAsync 執行 await後 ThreadId：{Thread.CurrentThread.ManagedThreadId}");
+            Console.WriteLine($"{nameof(AwaitMethod_A_Async)} 執行 await後 ThreadId：{Thread.CurrentThread.ManagedThreadId}");
             return result;
         }
 
-        public static async Task<string> AwaitMethodBAsync(int no)
+        public static async Task<string> AwaitMethod_B_Async(int no)
         {
-            Console.WriteLine($"AwaitMethodBAsync 執行 await前 ThreadId：{Thread.CurrentThread.ManagedThreadId}");
+            Console.WriteLine($"{nameof(AwaitMethod_B_Async)} 執行 await前 ThreadId：{Thread.CurrentThread.ManagedThreadId}");
             string result = await GetMillisecondAsync(no);
-            Console.WriteLine($"AwaitMethodBAsync 執行 await後 ThreadId：{Thread.CurrentThread.ManagedThreadId}");
+            Console.WriteLine($"{nameof(AwaitMethod_B_Async)} 執行 await後 ThreadId：{Thread.CurrentThread.ManagedThreadId}");
             return result;
         }
 
-        public static async Task<string> AwaitMethodCAsync(int no)
+        public static async Task<string> AwaitMethod_C_Async(int no)
         {
-            Console.WriteLine($"AwaitMethodCAsync 執行 await前 ThreadId：{Thread.CurrentThread.ManagedThreadId}");
+            Console.WriteLine($"{nameof(AwaitMethod_C_Async)} 執行 await前 ThreadId：{Thread.CurrentThread.ManagedThreadId}");
             string result = await GetMillisecondAsync(no);
-            Console.WriteLine($"AwaitMethodCAsync 執行 await後 ThreadId：{Thread.CurrentThread.ManagedThreadId}");
+            Console.WriteLine($"{nameof(AwaitMethod_C_Async)} 執行 await後 ThreadId：{Thread.CurrentThread.ManagedThreadId}");
             return result;
         }
     }
