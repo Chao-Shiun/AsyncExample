adb012cde536e44736a7a630eca9addc7000310c
Charles <e4366.tw@gmail.com>
2019-07-05 18:01:15 +0800

    範例調整

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -8,17 +10,44 @@ namespace ConsoleApp4
     {
         static async Task Main(string[] args)
         {
-            //List<string> list = new List<string>();
-            //for (int i = 1; i <= 10; i++)
-            //{
-            //    list.Add(GetMillisecond(i));
-            //}
+            #region General sync program
+            List<string> list = new List<string>();
+            for (int i = 1; i <= 10; i++)
+            {
+                list.Add(GetMillisecond(i));
+            }
 
-            //foreach (var item in list)
-            //{
-            //    Console.WriteLine($"{item}");
-            //}
+            foreach (var item in list)
+            {
+                Console.WriteLine($"{item}");
+            }
+            #endregion
 
+            #region Many await sync flow
+            //Console.WriteLine($"準備執行{nameof(AwaitMethod_A_Async)}");
+            //var result1 = await AwaitMethod_A_Async(1);
+            //Console.WriteLine($"準備執行{nameof(AwaitMethod_B_Async)}");
+            //var result2 = await AwaitMethod_B_Async(2);
+            //Console.WriteLine($"準備執行{nameof(AwaitMethod_C_Async)}");
+            //var result3 = await AwaitMethod_C_Async(3);
+
+            //Console.WriteLine(result1);
+            //Console.WriteLine(result2);
+            //Console.WriteLine(result3);
+            #endregion
+
+            #region Many await async flow
+            //Console.WriteLine($"準備執行{nameof(AwaitMethod_A_Async)}");
+            //var result1 = AwaitMethod_A_Async(1);
+            //Console.WriteLine($"準備執行{nameof(AwaitMethod_B_Async)}");
+            //var result2 = AwaitMethod_B_Async(2);
+            //Console.WriteLine($"準備執行{nameof(AwaitMethod_C_Async)}");
+            //var result3 = AwaitMethod_C_Async(3);
+
+            //Console.WriteLine(await result1);
+            //Console.WriteLine(await result2);
+            //Console.WriteLine(await result3);
+            #endregion
 
             #region Whenall
             //List<Task<string>> taskList = new List<Task<string>>();
@@ -31,13 +60,12 @@ namespace ConsoleApp4
 
             //Console.WriteLine($"\nafter leave loop Thread.CurrentThread.ManagedThreadId:{Thread.CurrentThread.ManagedThreadId}\n");
 
-            //await Task.WhenAll(taskList);
+            //_ = await Task.WhenAll(taskList);
             //foreach (var item in taskList)
             //{
             //    Console.WriteLine($"{await item}");
             //}
             #endregion
-
 
             #region Whenany
             //List<Task<string>> taskList = new List<Task<string>>();
@@ -57,38 +85,6 @@ namespace ConsoleApp4
             //}
             #endregion
 
-
-            #region Many await sync flow
-            Console.WriteLine($"準備執行{nameof(AwaitMethod_A_Async)}");
-            var result1 = await AwaitMethod_A_Async(1);
-            Console.WriteLine($"準備執行{nameof(AwaitMethod_B_Async)}");
-            var result2 = await AwaitMethod_B_Async(2);
-            Console.WriteLine($"準備執行{nameof(AwaitMethod_C_Async)}");
-            var result3 = await AwaitMethod_C_Async(3);
-
-            Console.WriteLine(result1);
-            Console.WriteLine(result2);
-            Console.WriteLine(result3);
-
-
-            #endregion
-
-
-            #region Many await async flow
-            //Console.WriteLine($"準備執行{nameof(AwaitMethod_A_Async)}");
-            //var result1 = AwaitMethod_A_Async(1);
-            //Console.WriteLine($"準備執行{nameof(AwaitMethod_B_Async)}");
-            //var result2 = AwaitMethod_B_Async(2);
-            //Console.WriteLine($"準備執行{nameof(AwaitMethod_B_Async)}");
-            //var result3 = AwaitMethod_C_Async(3);
-
-            //var waitAll = Task.WhenAll(result1, result2, result3);
-
-            //Console.WriteLine(await result1);
-            //Console.WriteLine(await result2);
-            //Console.WriteLine(await result3);
-
-            #endregion
             Console.ReadKey();
         }
 
@@ -97,20 +93,21 @@ namespace ConsoleApp4
             return Task.Run(() =>
             {
                 Console.WriteLine($"inner task No:{no}  Thread.CurrentThread.ManagedThreadId:{Thread.CurrentThread.ManagedThreadId}");
-                Thread.Sleep(new Random(Guid.NewGuid().GetHashCode()).Next(10000, 20000));
-                return DateTime.Now.ToString($"{no}. mm:ss fff");
+                Thread.Sleep(new Random(Guid.NewGuid().GetHashCode()).Next(5000, 10000));
+                return DateTime.Now.ToString($"{no}. mm:ss fffffff");
             });
         }
 
         public static string GetMillisecond(int no)
         {
-            return DateTime.Now.ToString($"{no} mm:ss fff");
+            //Thread.Sleep(100);
+            return DateTime.Now.ToString($"{no} mm:ss fffffff");
         }
 
         public static async Task<string> AwaitMethod_A_Async(int no)
         {
             Console.WriteLine($"{nameof(AwaitMethod_A_Async)} 執行 await前 ThreadId：{Thread.CurrentThread.ManagedThreadId}");
-            string result = await GetMillisecondAsync(no);
+            string result = await GetMillisecondAsync(no).ConfigureAwait(false);
             Console.WriteLine($"{nameof(AwaitMethod_A_Async)} 執行 await後 ThreadId：{Thread.CurrentThread.ManagedThreadId}");
             return result;
         }
@@ -118,7 +115,7 @@ namespace ConsoleApp4
         public static async Task<string> AwaitMethod_B_Async(int no)
         {
             Console.WriteLine($"{nameof(AwaitMethod_B_Async)} 執行 await前 ThreadId：{Thread.CurrentThread.ManagedThreadId}");
-            string result = await GetMillisecondAsync(no);
+            string result = await GetMillisecondAsync(no).ConfigureAwait(false);
             Console.WriteLine($"{nameof(AwaitMethod_B_Async)} 執行 await後 ThreadId：{Thread.CurrentThread.ManagedThreadId}");
             return result;
         }
@@ -126,7 +123,7 @@ namespace ConsoleApp4
         public static async Task<string> AwaitMethod_C_Async(int no)
         {
             Console.WriteLine($"{nameof(AwaitMethod_C_Async)} 執行 await前 ThreadId：{Thread.CurrentThread.ManagedThreadId}");
-            string result = await GetMillisecondAsync(no);
+            string result = await GetMillisecondAsync(no).ConfigureAwait(false);
             Console.WriteLine($"{nameof(AwaitMethod_C_Async)} 執行 await後 ThreadId：{Thread.CurrentThread.ManagedThreadId}");
             return result;
         }
